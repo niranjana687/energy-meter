001b73d4980020368a3feba22dd85ebc2af4668f
Niranjana <niranjana687>
2020-04-17 17:24:46 +0530

    api tryout

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module EnergyMeter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    
+    end
+
+    
+end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,3 +22,4 @@ module EnergyMeter
     # the framework and any gems in your application.
   end
 end
+
